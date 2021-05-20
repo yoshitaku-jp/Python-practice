b2dd4c66aa3b7ad64636a36ad5ca02c02542bae9
yoshitaku-jp <yoshimura.takuya1990@gmail.com>
2021-05-21 08:54:36 +0900

    change methods

--- lib/rgb.py
+++ lib/rgb.py
@@ -8,4 +8,10 @@ def to_hex(r, g, b):
 
 
 def to_ints(hex):
-    return [0, 0, 0]
+    r = hex[1:3]
+    g = hex[3:5]
+    b = hex[5:7]
+    ints = []
+    for i in [r, g, b]:
+        ints.append(int(i, base=16))
+    return ints
