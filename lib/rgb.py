f8440f91c9b0eb59ddffe4f2e30dba7b88aecfc2
yoshitaku-jp <yoshimura.takuya1990@gmail.com>
2021-05-20 23:49:26 +0900

    add ints method

--- lib/rgb.py
+++ lib/rgb.py
@@ -5,3 +5,7 @@ def to_hex(r, g, b):
         hex += format(x, "x").rjust(2, "0")
 
     return hex
+
+
+def to_ints(hex):
+    return [0, 0, 0]
--- tests/test_rgb.py
+++ tests/test_rgb.py
@@ -1,9 +1,14 @@
 import unittest
 
-from lib.rgb import to_hex
+from lib.rgb import to_hex, to_ints
 
 
 class TestRgb(unittest.TestCase):
     def test_to_hex(self):
         self.assertEqual("#000000", to_hex(0, 0, 0))
         self.assertEqual("#ffffff", to_hex(255, 255, 255))
+        self.assertEqual("#043c78", to_hex(4, 60, 120))
+
+    def test_to_ints(self):
+        self.assertEqual([0, 0, 0], to_ints("#000000"))
+        self.assertEqual([255, 255, 255], to_ints("#ffffff"))
