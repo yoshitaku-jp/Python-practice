6f26e5fd0f21c5adab9a5d64b06a216e61bf8b16
yoshitaku-jp <yoshimura.takuya1990@gmail.com>
2021-05-22 22:58:04 +0900

    refactoring

--- lib/rgb.py
+++ lib/rgb.py
@@ -11,7 +11,4 @@ def to_ints(hex):
     r = hex[1:3]
     g = hex[3:5]
     b = hex[5:7]
-    ints = []
-    for i in [r, g, b]:
-        ints.append(int(i, base=16))
-    return ints
+    return list(map(lambda x: int(x, base=16), [r, g, b]))
--- tests/test_rgb.py
+++ tests/test_rgb.py
@@ -12,3 +12,4 @@ class TestRgb(unittest.TestCase):
     def test_to_ints(self):
         self.assertEqual([0, 0, 0], to_ints("#000000"))
         self.assertEqual([255, 255, 255], to_ints("#ffffff"))
+        self.assertEqual([4, 60, 120], to_ints("#043c78"))
