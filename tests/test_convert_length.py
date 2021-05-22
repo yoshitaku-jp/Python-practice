b01586788a9de617e72c46a33957a070c937e12b
yoshitaku-jp <yoshimura.takuya1990@gmail.com>
2021-05-23 08:51:13 +0900

    add test case

--- tests/test_convert_length.py
+++ tests/test_convert_length.py
@@ -6,3 +6,4 @@ from package.convert_length import convert_length
 class TestConvertLengthTest(unittest.TestCase):
     def test_convert_length(self):
         self.assertEqual(39.37, convert_length(1, "m", "in"))
+        self.assertEqual(0.38, convert_length(15, "in", "m"))
