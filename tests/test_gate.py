641392be610ca436fd61020845520bab9a742a19
yoshitaku-jp <yoshimura.takuya1990@gmail.com>
2021-05-25 07:44:21 +0900

    add test case 'Gate'

--- tests/test_gate.py
+++ tests/test_gate.py
@@ -5,4 +5,9 @@ from package import gate
 
 class TestGate(unittest.TestCase):
     def test_gate(self):
-        self.assertTrue(gate.Gate())
+
+        umeda = gate.Gate(umeda)
+        juso = gate.Gate(juso)
+        ticket = ticket.Ticket(150)
+        umeda.entry(ticket)
+        self.assertTrue(juso.exit(ticket))
