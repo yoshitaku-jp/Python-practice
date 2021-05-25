a2e5db43e49ebbd74177afb052d79f9e9a4ebf24
yoshitaku-jp <yoshimura.takuya1990@gmail.com>
2021-05-26 07:07:46 +0900

    add test case

--- tests/test_gate.py
+++ tests/test_gate.py
@@ -1,13 +1,21 @@
 import unittest
 
-from package import gate
+from package import gate, ticket
 
 
 class TestGate(unittest.TestCase):
     def test_gate(self):
 
-        umeda = gate.Gate(umeda)
-        juso = gate.Gate(juso)
-        ticket = ticket.Ticket(150)
-        umeda.entry(ticket)
-        self.assertTrue(juso.exit(ticket))
+        umeda = gate.Gate("umeda")
+        juso = gate.Gate("juso")
+        ticket1 = ticket.Ticket(150)
+        umeda.enter(ticket1)
+        self.assertTrue(juso.exit(ticket1))
+
+    def test_umeda_to_mikuni_when_fare_is_not_enough(self):
+        umeda = gate.Gate("umeda")
+        mikuni = gate.Gate("juso")
+
+        ticket1 = ticket.Ticket(150)
+        umeda.enter(ticket1)
+        self.assertFalse(mikuni.exit(ticket1))
